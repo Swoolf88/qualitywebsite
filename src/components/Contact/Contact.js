fe71a69c77158c9faa0d02ac41631450f6a0c54b
Farkas Istvan <farkasistvan42@gmail.com>
2023-10-09 19:49:51 +0200

    fixing
    fixing some issues

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
   return ( 
     <>
     <p className="contacttext">
-      Központi email címünk:<br></br> lakatos.ruzsicska@gmail.com<br></br><br></br>
+      Központi email címünk:<br></br>lakatos.ruzsicska @gmail.com<br></br><br></br>
 
 Lakatos Gyula:<br></br>
 Gépészmérnök <br></br>
@@ -13,7 +13,7 @@ Rendszerépítő Auditor<br></br>
 +36/30 473 5926<br></br>
 <br></br>
 Ruzsicska István:<br></br>
-Anyagmérnök/Környezetmérnök<br></br>
+Anyagmérnök / Környezetmérnök<br></br>
 Vezető Auditor<br></br>
 +36/70 508 6613<br></br>
     </p>
